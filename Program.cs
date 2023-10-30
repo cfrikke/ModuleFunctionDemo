1284246715170e8e529343c1820b89483e9fa71d
Coco <143611879+cfrikke@users.noreply.github.com>
2023-10-30 13:03:32 +0000

    Demo  Complete! :)

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿    Console.Clear();
+    Console.WriteLine("Please input your name.");
+    string n;
+    n = Console.ReadLine();
+    GreetUser(n);
+
+
+static void GreetUser(string Name){
+    Console.WriteLine("Hello " + Name + ".");
+}
